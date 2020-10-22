1410b5a90a49d50a1b040846193519a0b56a0cda
gen-ghub <renbenzhuang234@gmail.com>
2020-10-22 22:12:42 +0900

    配列の順番を変更せずに実装

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,7 +19,7 @@ class CalendarsController < ApplicationController
   end
 
   def get_week
-    wdays = ['(木)','(金)','(土)','(日)','(月)','(火)','(水)']
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -34,11 +34,12 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+      wday_num = Date.today.wday + x
 
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans,wday: wdays[x]}
-
-
-
+      if wday_num >= 7
+        wday_num = wday_num - 7
+      end
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
 
